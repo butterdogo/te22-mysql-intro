6869e684be9f596f9dbb182b5f5fd9c46c21a6d3
olle <olle.axelsson@elev.ga.ntig.se>
2024-11-26 11:12:56 +0100

    äckel

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
-import 'dotenv/config'
-import express from 'express'
-import pool from './db.js'
+import "dotenv/config"
+import nunjucks from "nunjucks"
+import express from "express"
+import pool from "./db.js"
 
 const app = express()
 const port = 3000
 
-app.get('/', async (req, res) => {
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app,
+})
 
-    const [birds] = await pool.promise().query("SELECT * FROM birds")
-
+app.get("/birds", async (req, res) => {
+    // const [birds] = await pool.promise().query('SELECT * FROM birds')
+    const [birds] = await pool
+        .promise()
+        .query(
+            "SELECT birds.*, species.name AS species FROM birds JOIN species ON birds.species_id = species.id;",
+        )
     res.json(birds)
 })
 
+app.get("/birds/:id", async (req, res) => {
+    const [bird] = await pool
+        .promise()
+        .query(
+            "SELECT birds.*, species.name AS species FROM birds JOIN species ON birds.species_id = species.id WHERE birds.id = ?;",
+            [req.params.id],
+        )
+    res.json(bird[0])
+})
+
+app.get("/birdsnjk", async (req, res) => {
+
+    const [birds] = await pool
+        .promise()
+        .query(
+            "SELECT birds.*, species.name AS species FROM birds JOIN species ON birds.species_id = species.id;",
+        )
+
+    res.render("birds.njk", {
+        birds
+    })
+
+})
+
 app.listen(port, () => {
-
-    console.log(`Exampel app listening at https://localhost:${port}` )
-
+    console.log(`Example app listening at http://localhost:${port}`)
 })
