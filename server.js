dd05106d75a4ed75cbca2f6cfd66c56a01484d4f
olle <olle.axelsson@elev.ga.ntig.se>
2024-12-10 11:08:52 +0100

    forum

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import "dotenv/config"
 import nunjucks from "nunjucks"
 import express from "express"
 import pool from "./db.js"
+import morgan from "morgan"
+import bodyParser from "body-parser"
 
 const app = express()
 const port = 3000
@@ -10,6 +12,11 @@ nunjucks.configure("views", {
     autoescape: true,
     express: app,
 })
+
+
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({extenden: true}));
+app.use(bodyParser.json());
 
 app.use(express.static("public"))
 
@@ -38,7 +45,51 @@ app.get("/birds/:id", async (req, res) => {
     console.log(bird)
 })
 
+app.get("/birds_form", async (req, res) => {
+    const [species] = await pool.promise().query('SELECT * FROM species')
+    
+res.render('birds_form.njk', {
+        species,
+        title: "New bird"
+    })
+})
 
+app.get("/species_form", async (req, res) => {
+
+    res.render('species_form.njk', {
+    
+    })
+})
+app.get('/birds/new', async (req, res) => {
+    const [species] = await pool.promise().query('SELECT * FROM species')
+  
+    res.render('birds_form.njk', { species })
+})
+
+app.post('/species', async (req, res) => {
+    const { name, latin, wingspan_min, wingspan_max } = req.body
+  
+    const [result] = await pool.promise().query('INSERT INTO species (name, latin, wingspan_min, wingspan_max) VALUES (?, ?, ?, ?)', [name, latin, wingspan_min, wingspan_max])
+  
+    res.render('newspecies.njk', {
+
+    })
+  })
+
+app.get('/birds/new', async (req, res) => {
+  const [species] = await pool.promise().query('SELECT * FROM species')
+
+  res.render('birds_form.njk', { species })
+})
+
+app.post('/birds', async (req, res) => {
+  console.log(req.body)
+  const { name, wingspan, species_id } = req.body
+
+  const [result] = await pool.promise().query('INSERT INTO birds (name, wingspan, species_id) VALUES (?, ?, ?)', [name, wingspan, species_id])
+
+  res.json(result)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
